a230979f1796f110234edb9af9e8850c1329a9cc
Badal <surajbadal69@gmail.com>
2024-10-20 16:11:02 +0530

    update the apikey handle

--- src/component/MainContainer.jsx
+++ src/component/MainContainer.jsx
@@ -4,13 +4,14 @@ import '../styles/MainContainer.css'
 import axios from 'axios'
 import Card from './Card'
 
+const apikey = import.meta.env.VITE_API_KEY;
 function MainContainer() {
     const [movieName, setMovieName] = useState('');
     const [movieDetails, setMovieDetails] = useState({})
     const [isvalidRequest, setIsValidRequest] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const apikey = import.meta.env.VITE_API_KEY;
+
     const movieRequest = () => {
         let request = `http://www.omdbapi.com/?apikey=${apikey}&t=${movieName}`;
         axios.get(request)
